5b6fd6f5ccdf3abf614df3d2107be8e8439cc8ac
Serhii Smirnov <snoff87@gmail.com>
2023-08-12 18:07:17 +0300

    task2

--- task.py
+++ task.py
@@ -4,6 +4,7 @@ CENTER_X = 320
 CENTER_Y = 256
 POINT_X = 558
 POINT_Y = 328
+AZ = 335
 
 delta_x = (CENTER_X - POINT_X)**2
 delta_y = (CENTER_Y - POINT_Y)**2
@@ -12,9 +13,11 @@ distance = distance_in_pixels * 0.38
 
 angle_radian = math.atan2(delta_y, delta_x)
 angle_degrees = angle_radian*(180/math.pi)
+point_az = 90 + angle_degrees - (360 - AZ)
 
-print(distance)
-print(angle_degrees)
+print(distance, "meters")
+print(angle_degrees,"degrees")
+print(point_az, "degrees")
 
 
 
