9f19d6aa96025af4cda3470f7309c99e8837361c
Serhii Smirnov <snoff87@gmail.com>
2023-09-04 16:45:59 +0300

    new approach again

--- fly_lib.py
+++ fly_lib.py
@@ -1,11 +1,15 @@
 import time
-
+from threading import Timer
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil
 
 class Copter:
     def __init__(self):
         self.armed = 0
+        self.mav_alt = 0
+        self.vz = 0
+        self.pitch = 1500
+        self.yaw = 1500
     def connect(self):
         print('+++++++++++++++++')
         print('CONNECTING !!!')
@@ -34,31 +38,30 @@ class Copter:
         print('ARMED !!!')
         print('+++++++++++++++++')
 
-    def fly_up(self, need_alt):
+    def update(self):
         if (self.vehicle.armed):
-            while True:
-                try:
-                    mav_alt = self.vehicle.location.global_relative_frame.alt
-                except:
-                    pass
-                if (mav_alt is None): mav_alt = 0
-                print('+++++++++++++++++++++++++')
-                print(mav_alt)
-                print('======= GO UP !!! =======')
-                print('+++++++++++++++++++++++++')
 
-                d_alt = mav_alt - need_alt
+            try:
+                self.mav_alt = self.vehicle.location.global_relative_frame.alt
 
-                if (d_alt < -12):
-                    # Need UP
-                    vz = 1500
-                elif (-5 < d_alt < 0):
-                    # Need UP
-                    vz = 1480
-                elif (d_alt > 0):
-                    # Need Down
-                    vz = 1478
-                    right_height = True
+            except:
+                pass
 
-                self.vehicle.channels.overrides = {'3': vz}
-                time.sleep(0.8)
+            self.vehicle.channels.overrides = {'2': self.pitch, '3': self.vz, '4': self.yaw,}
+            print(self.vz)
+            Timer(0.8, self.update).start()
+    def fly_up(self, need_alt):
+
+        while True:
+            d_alt = self.mav_alt - need_alt
+            print(d_alt)
+            if (d_alt < -12):
+                # Need UP
+                self.vz = 1500
+            elif (-5 < d_alt < 0):
+                # Need UP
+                self.vz = 1480
+            elif (d_alt > 0):
+                # Need Down
+                self.vz = 1478
+            time.sleep(0.8)
